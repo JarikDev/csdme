cd4ab1c0aaf8366a4bc2229ad7d295e8025e200a
ivan4000 <sxln1@mail.ru>
2019-02-22 06:45:33 +0300

    Basic realization

--- src/main/java/checkservice/service/CheckServiceApplication.java
+++ src/main/java/checkservice/service/CheckServiceApplication.java
@@ -1,3 +1,64 @@
 package checkservice.service;
 
-public class CheckServiceApplication {}
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class CheckServiceApplication {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CheckServiceConfiguration.class);
+        CheckServiceClient checkServiceClient = context.getBean(CheckServiceClient.class);
+        NdsResponse2 ndsResponse2 = checkServiceClient.getNdsResponse2();
+        System.out.println("####################################");
+        System.out.println(ndsResponse2.getNP().get(0).getDT());
+        System.out.println(ndsResponse2.getNP().get(0).getINN());
+        System.out.println(ndsResponse2.getNP().get(0).getKPP());
+        System.out.println(ndsResponse2.getNP().get(0).getState());
+        System.out.println("####################################");
+        int responseStatus = Integer.parseInt(ndsResponse2.getNP().get(0).getState());
+        switch (responseStatus) {
+            case 0:
+                System.out.println("Налогоплательщик зарегистрирован в ЕГРН и имел статус действующего в указанную дату");
+                break;
+            case 1:
+                System.out.println("Налогоплательщик зарегистрирован в ЕГРН, но не имел статус действующего в указанную дату");
+                break;
+            case 2:
+                System.out.println("Налогоплательщик зарегистрирован в ЕГРН");
+                break;
+            case 3:
+                System.out.println("Налогоплательщик с указанным ИНН зарегистрирован в ЕГРН, КПП не соответствует ИНН или не указан");
+                break;
+            case 4:
+                System.out.println("Налогоплательщик с указанным ИНН не зарегистрирован в ЕГРН");
+                break;
+            case 5:
+                System.out.println("Некорректный ИНН");
+                break;
+            case 6:
+                System.out.println("Недопустимое количество символов ИНН");
+                break;
+            case 7:
+                System.out.println("Недопустимое количество символов КПП");
+                break;
+            case 8:
+                System.out.println("Недопустимые символы в ИНН");
+                break;
+            case 9:
+                System.out.println("Недопустимые символы в КПП");
+                break;
+            case 10:
+                System.out.println("КПП не должен использоваться при проверке ИП");
+                break;
+            case 11:
+                System.out.println("некорректный формат даты");
+                break;
+            case 12:
+                System.out.println("некорректная дата (ранее 01.01.1991 или позднее текущей даты)");
+                break;
+
+        }
+
+    }
+}
--- src/main/java/checkservice/service/CheckServiceConfiguration.java
+++ src/main/java/checkservice/service/CheckServiceConfiguration.java
@@ -1,3 +1,26 @@
 package checkservice.service;
 
-public class CheckServiceConfiguration {}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class CheckServiceConfiguration {
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // this package must match the package in the <generatePackage> specified in
+        // pom.xml
+        marshaller.setContextPath("checkservice.service");
+        return marshaller;
+    }
+
+    @Bean
+    public CheckServiceClient checkServiceClient(Jaxb2Marshaller marshaller) {
+        CheckServiceClient client = new CheckServiceClient();
+        client.setDefaultUri("http://localhost:8080/ws");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+}
